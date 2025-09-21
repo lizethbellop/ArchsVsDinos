94841f21290c213ecdb1eb96bfab72259f247d7f
Liz Bello <blizethguadalupe@gmail.com>
2025-09-20 20:52:54 -0600

    login listo, falta el controlador

---
+++ ArchsVsDinosClient/ArchsVsDinosClient/Utils/ValidationHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArchsVsDinosClient.Utils
+{
+    public static class ValidationHelper
+    {
+
+        public static bool isEmpty(string input)
+        {
+            return string.IsNullOrEmpty(input);
+        }
+    }
+}
