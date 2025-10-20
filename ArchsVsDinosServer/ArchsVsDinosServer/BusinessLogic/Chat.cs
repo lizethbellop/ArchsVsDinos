b626957230458b9bfa0c787746a662335601f57f
Liz Bello <blizethguadalupe@gmail.com>
2025-10-20 09:48:22 -0600

    Inicio de chat

---
+++ ArchsVsDinosServer/ArchsVsDinosServer/BusinessLogic/Chat.cs
@@ -0,0 +1,34 @@
+﻿using Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArchsVsDinosServer.BusinessLogic
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    public class Chat : IChatManager
+    {
+        public void Connect(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Disconnect(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendMessageToRoom(string roomId, string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendMessageToUser(string targetUser, string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ArchsVsDinosServer/Contracts/IChatManagerCallback.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts
+{
+    public interface IChatManagerCallback
+    {
+        [OperationContract(IsOneWay = true)]
+        void ReceiveMessage(string roomId, string fromUser, string message);
+
+        [OperationContract(IsOneWay = true)]
+        void ReceiveSystemNotification(string notification);
+    }
+}
