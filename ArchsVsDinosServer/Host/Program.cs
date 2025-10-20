f6af157266b26ceecd51e45debd4c6770e6fc451
AbrahamC13 <canoabraham172@gmail.com>
2025-10-20 17:40:25 -0600

    cambios en Host

--- ArchsVsDinosServer/Host/Program.cs
+++ ArchsVsDinosServer/Host/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ServiceModel;
+using ArchsVsDinosServer.Services;
 
 namespace Host
 {
@@ -10,6 +12,31 @@ namespace Host
     {
         static void Main(string[] args)
         {
+
+            using (ServiceHost registerHost = new ServiceHost(typeof(RegisterManager)))
+            using (ServiceHost authenticationHost = new ServiceHost(typeof(AuthenticationManager)))
+            using (ServiceHost profileHost = new ServiceHost(typeof(ProfileManager)))
+            {
+                try
+                {
+                    registerHost.Open();
+                    authenticationHost.Open();
+                    profileHost.Open();
+                    Console.WriteLine("Server is running");
+                    Console.ReadLine();
+                }
+                catch (CommunicationException ce)
+                {
+                    Console.WriteLine("Error starting services: ", ce.Message);
+
+                    registerHost.Abort();
+                    authenticationHost.Abort();
+                    profileHost.Abort();
+                }
+
+            }
+
         }
+
     }
 }
