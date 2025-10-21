66aee5b363efbdace0d54aa347ebca03cc7c705c
Liz Bello <blizethguadalupe@gmail.com>
2025-10-21 10:44:13 -0600

    chat

--- ArchsVsDinosServer/ArchsVsDinosServer/Services/ChatManager.cs
+++ ArchsVsDinosServer/ArchsVsDinosServer/Services/ChatManager.cs
@@ -1,12 +1,36 @@
-﻿using System;
+﻿using Contracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
+using ArchsVsDinosServer.BusinessLogic;
 
 namespace ArchsVsDinosServer.Services
 {
-    internal class ChatManager
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single, ConcurrencyMode = ConcurrencyMode.Multiple)]
+    public class ChatManager : IChatManager
     {
+        private Chat chatBusinessLogic;
+        public void Connect(string username)
+        {
+            chatBusinessLogic.Connect(username);
+        }
+
+        public void Disconnect(string username)
+        {
+            chatBusinessLogic.Disconnect(username);
+        }
+
+        public void SendMessageToRoom(string message, string username)
+        {
+            chatBusinessLogic.SendMessageToRoom(message, username);
+        }
+
+        public void SendMessageToUser(string username, string targetUser, string message)
+        {
+            chatBusinessLogic.SendMessageToUser(username, targetUser, message);
+        }
     }
 }
