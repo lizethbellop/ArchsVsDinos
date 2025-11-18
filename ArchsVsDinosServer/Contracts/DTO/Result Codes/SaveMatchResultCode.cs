a4edc74ec08c912cfca8fbe0460e197db8df2956
Liz Bello <blizethguadalupe@gmail.com>
2025-11-17 22:53:14 -0600

    Inicio de desarrollo de estadísticas

---
+++ ArchsVsDinosServer/Contracts/DTO/Result Codes/SaveMatchResultCode.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts.DTO.Result_Codes
+{
+    [DataContract]
+    public enum SaveMatchResultCode
+    {
+        [EnumMember]
+        Success,
+        [EnumMember]
+        MatchNotFound,
+        [EnumMember]
+        InvalidData,
+        [EnumMember]
+        DatabaseError,
+        [EnumMember]
+        UnexpectedError
+    }
+}
---
+++ ArchsVsDinosServer/Contracts/DTO/Statistics/MatchHistoryDTO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts.DTO.Statistics
+{
+    [DataContract]
+    public class MatchHistoryDTO
+    {
+        [DataMember]
+        public int MatchId { get; set; }
+
+        [DataMember]
+        public DateTime MatchDate { get; set; }
+
+        [DataMember]
+        public int Points { get; set; }
+
+        [DataMember]
+        public bool Won { get; set; }
+
+        [DataMember]
+        public int Position { get; set; }
+
+        [DataMember]
+        public int TotalPlayers { get; set; }
+    }
+}
