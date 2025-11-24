9601908d0f78851f1582ed85b50b811d4c2698f6
Liz Bello <blizethguadalupe@gmail.com>
2025-11-24 12:22:34 -0600

    si

---
+++ ArchsVsDinosServer/ArchsVsDinosServer/Services/Interfaces/IGameNotifier.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArchsVsDinosServer.Services.Interfaces
+{
+    public interface IGameNotifier
+    {
+        void NotifyPlayerExpelled(string matchCode, string username, string reason);
+        void NotifyGameClosure(string matchCode, string reason);
+    }
+}
---
+++ ArchsVsDinosServer/ArchsVsDinosServer/Services/Interfaces/ILobbyNotifier.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArchsVsDinosServer.Services.Interfaces
+{
+    public interface ILobbyNotifier
+    {
+        void NotifyPlayerExpelled(string username, string reason);
+        void NotifyLobbyClosure(string reason);
+    }
+}
