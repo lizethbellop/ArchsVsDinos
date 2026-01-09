649c5f65ad6b4aef3536701508ca7379a75cb58b
AbrahamC13 <canoabraham172@gmail.com>
2026-01-09 15:31:49 -0600

    cambios instrucciones y mainwindow

---
+++ ArchsVsDinosClient/ArchsVsDinosClient/Views/Instructions/Instructions.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ArchsVsDinosClient.Views.Instructions
+{
+    public partial class Instructions : Window
+    {
+        public Instructions()
+        {
+            InitializeComponent();
+            MainFrame.Navigate(new Page1());
+        }
+
+        private void Click_BtnClose(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
